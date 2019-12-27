0266a4b90723194cbcfa83b24dea6164722201c4
Jason Wong <jasonwong@vgt-hk.net>
2019-12-27 17:29:52 +0800

    Added objc file

---
+++ FYP/Controller/NetworkCall.m
@@ -0,0 +1,17 @@
+//
+//  NetworkCall.m
+//  FYP
+//
+//  Created by Jason Wong on 27/12/2019.
+//  Copyright © 2019 Jason Wong. All rights reserved.
+//
+
+#import "NetworkCall.h"
+
+@implementation NetworkCall 
+
+- (void) downloadJson {
+    NSLog(@"Downloading data from Json");
+}
+
+@end
---
+++ FYP/NetworkCall.m
@@ -0,0 +1,9 @@
+//
+//  NetworkCall.m
+//  FYP
+//
+//  Created by Jason Wong on 27/12/2019.
+//  Copyright © 2019 Jason Wong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
